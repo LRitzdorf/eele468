363c22a13ce502313c2659083e7d45bb437e147d
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-01-26 00:59:53 -0700

    adc: add shift register IP components

--- adc-controller/adc-controller.vhd
+++ adc-controller/adc-controller.vhd
@@ -30,6 +30,44 @@ end entity;
 -- ADC controller implementation
 architecture ADC_Controller_Arch of ADC_Controller is
 
+    signal shift_clk : std_logic;
+    -- Config shift register (output) signals
+    signal config_data : std_logic_vector(5 downto 0);
+    signal config_load : std_logic;
+
+    -- IP components
+    component shiftreg_s2p is
+        port (
+            clock   : in std_logic;
+            shiftin : in std_logic;
+            q       : out std_logic_vector(11 downto 0)
+        );
+    end component;
+    component shiftreg_p2s is
+        port (
+            clock    : in std_logic;
+            data     : in std_logic_vector(5 downto 0);
+            load     : in std_logic;
+            shiftout : out std_logic
+        );
+    end component;
+
 begin
+
+    -- Shift registers
+    -- Capture serial input data into parallel vector
+    shiftreg_s2p_inst : shiftreg_s2p port map (
+        clock   => shift_clk,
+        shiftin => sdo,
+        q       => sample
+    );
+    -- Transmit configuration vector in serial form
+    shiftreg_p2s_inst : shiftreg_p2s port map (
+        clock    => shift_clk,
+        data     => config_data,
+        load     => config_load,
+        shiftout => sdi
+    );
+    sck <= shift_clk;   -- Pass shift clock to ADC
 
 end architecture;
