560f53f46ceeff3b3cea2c84484fc976377e8f1c
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-04-01 00:44:41 -0600

    fft: apply personalized cutoff frequencies

--- matlab/fft-analysis/createFFTFilters.m
+++ matlab/fft-analysis/createFFTFilters.m
@@ -16,7 +16,7 @@ modelParams.fftROMDataType = numerictype(1,16,8);
 %----------------------------------------------------------------------------
 % Filter 1 - Low Pass (filterSelect=0)
 %----------------------------------------------------------------------------
-modelParams.lowPassCutoff      = 4000;  % frequency in Hz
+modelParams.lowPassCutoff      = 5000;  % frequency in Hz
 modelParams.lowPassIndex       = floor(modelParams.lowPassCutoff / modelParams.audio.sampleFrequency * modelParams.fft.size) + 1;  % the +1 is to convert from the FFT bin array that is zero offset to a Matlab array that is 1 offset
 modelParams.fftGainsF1Real     = zeros(modelParams.fft.sizeHalf,1);  % start by zeroing the entire array
 modelParams.fftGainsF1Real(1:modelParams.lowPassIndex) = ones(modelParams.lowPassIndex,1);
@@ -27,8 +27,8 @@ modelParams.fftGainsF1Imag     = fi(modelParams.fftGainsF1Imag,modelParams.fftRO
 %----------------------------------------------------------------------------
 % Filter 2 - Band Pass (filterSelect=1)
 %----------------------------------------------------------------------------
-modelParams.bandPassCutoffLow  = 4000;  % frequency in Hz
-modelParams.bandPassCutoffHigh = 8000;  % frequency in Hz
+modelParams.bandPassCutoffLow  = 5000;  % frequency in Hz
+modelParams.bandPassCutoffHigh = 11000; % frequency in Hz
 modelParams.bandPassIndexLow   = ceil(modelParams.bandPassCutoffLow / modelParams.audio.sampleFrequency * modelParams.fft.size) + 1;  % Note: The FFT bin indexing assumes the first bin index number of zero.  Matlab starts arrays with index 1.  Thus we need to add get the right frequency bin.
 modelParams.bandPassIndexHigh  = floor(modelParams.bandPassCutoffHigh / modelParams.audio.sampleFrequency *modelParams.fft.size)+ 1;
 modelParams.fftGainsF2Real     = zeros(modelParams.fft.sizeHalf,1);  % start by zeroing array
@@ -40,7 +40,7 @@ modelParams.fftGainsF2Imag     = fi(modelParams.fftGainsF2Imag,modelParams.fftRO
 %----------------------------------------------------------------------------
 % Filter 3 - High Pass (filterSelect=2)
 %----------------------------------------------------------------------------
-modelParams.highPassCutoff     = 8000;  % frequency in Hz
+modelParams.highPassCutoff     = 11000; % frequency in Hz
 modelParams.highPassIndex      = ceil(modelParams.highPassCutoff / modelParams.audio.sampleFrequency * modelParams.fft.size) + 1;
 modelParams.fftGainsF3Real     = zeros(modelParams.fft.sizeHalf,1);  % start by zeroing array
 modelParams.fftGainsF3Real(modelParams.highPassIndex:end) = ones(modelParams.fft.sizeHalf - modelParams.highPassIndex + 1, 1);
